92dafcf23fbe1c283e6a6ed6c8b70ef588645c91
Gary Host <creadiv3@gmail.com>
2022-10-27 15:58:18 +0200

    Add accordion functionality to menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,27 +1,27 @@
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 
 {
-  ("use strict");
+  ('use strict');
 
   const select = {
     templateOf: {
-      menuProduct: "#template-menu-product",
+      menuProduct: '#template-menu-product',
     },
     containerOf: {
-      menu: "#product-list",
-      cart: "#cart",
+      menu: '#product-list',
+      cart: '#cart',
     },
     all: {
-      menuProducts: "#product-list > .product",
-      menuProductsActive: "#product-list > .product.active",
-      formInputs: "input, select",
+      menuProducts: '#product-list > .product',
+      menuProductsActive: '#product-list > .product.active',
+      formInputs: 'input, select',
     },
     menuProduct: {
-      clickable: ".product__header",
-      form: ".product__order",
-      priceElem: ".product__total-price .price",
-      imageWrapper: ".product__images",
-      amountWidget: ".widget-amount",
+      clickable: '.product__header',
+      form: '.product__order',
+      priceElem: '.product__total-price .price',
+      imageWrapper: '.product__images',
+      amountWidget: '.widget-amount',
       cartButton: '[href="#add-to-cart"]',
     },
     widgets: {
@@ -35,8 +35,8 @@
 
   const classNames = {
     menuProduct: {
-      wrapperActive: "active",
-      imageVisible: "active",
+      wrapperActive: 'active',
+      imageVisible: 'active',
     },
   };
 
@@ -55,35 +55,39 @@
   };
 
   const app = {
+    // loop over data product and create instance base on Product class
     initMenu() {
       const thisApp = this;
 
-      console.log("thisApp.data: ", thisApp.data);
+      // console.log('thisApp.data: ', thisApp.data);
 
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
       }
     },
 
+    // prepare data from data.js and store them in app property => this.App.data
     initData() {
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
 
+    // function where we initialize other func
     init() {
       const thisApp = this;
-      console.log("*** App starting ***");
-      console.log("thisApp:", thisApp);
-      console.log("classNames:", classNames);
-      console.log("settings:", settings);
-      console.log("templates:", templates);
+      // console.log('*** App starting ***');
+      // console.log('thisApp:', thisApp);
+      // console.log('classNames:', classNames);
+      // console.log('settings:', settings);
+      // console.log('templates:', templates);
 
       thisApp.initData();
       thisApp.initMenu();
     },
   };
 
+  // shema for our products
   class Product {
     constructor(id, data) {
       const thisProduct = this;
@@ -92,10 +96,12 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.initAccordion();
 
-      console.log("new Product:", thisProduct);
+      // console.log('new Product:', thisProduct);
     }
 
+    // using template handlebars to render menu product
     renderInMenu() {
       const thisProduct = this;
       // generate HTML based on template
@@ -106,6 +112,31 @@
       const menuContainer = document.querySelector(select.containerOf.menu);
       // add elem to menu
       menuContainer.appendChild(thisProduct.element);
+    }
+
+    // method to open and close choose option, quantity and add to cart button
+    initAccordion() {
+      const thisProduct = this;
+
+      const clickableTrigger = thisProduct.element.querySelector(
+        select.menuProduct.clickable
+      );
+
+      clickableTrigger.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const activeProduct = document.querySelector(
+          select.all.menuProductsActive
+        );
+
+        if (activeProduct && activeProduct !== thisProduct.element) {
+          activeProduct.classList.remove(classNames.menuProduct.wrapperActive);
+        }
+
+        thisProduct.element.classList.toggle(
+          classNames.menuProduct.wrapperActive
+        );
+      });
     }
   }
 

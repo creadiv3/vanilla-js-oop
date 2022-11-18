c4b31660ca48e706c3ebaacb4dd9fff1ce3ee5bf
Gary Host <creadiv3@gmail.com>
2022-11-18 12:47:21 +0100

    Add new page and possibility to switch between

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,65 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    const idFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id === idFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const clickedElement = this;
+
+        // get page id from href attribute
+        const id = clickedElement.getAttribute('href').replace('#', '');
+
+        // run thisApp.activatePage with that id
+        thisApp.activatePage(id);
+
+        // change URL hash
+        window.location.hash = '#/' + id;
+      });
+    }
+  },
+
+  activatePage: function (pageId) {
+    const thisApp = this;
+
+    // add class active to matching page, remove from non-matching
+    for (let page of thisApp.pages) {
+      // if (page.id === pageId) {
+      //   page.classList.add(classNames.pages.active);
+      // } else {
+      //   page.classList.remove(classNames.pages.active);
+      // }
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+    // add class active to matching links, remove from non-matching
+    for (let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') === '#' + pageId
+      );
+    }
+  },
   // loop over data product and create instance base on Product class
   initMenu() {
     const thisApp = this;
@@ -56,7 +113,7 @@ const app = {
     // console.log('classNames:', classNames);
     // console.log('settings:', settings);
     // console.log('templates:', templates);
-
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
